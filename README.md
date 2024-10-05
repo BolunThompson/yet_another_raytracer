dbd0d702a49121070d62162223927e709f0e2d67
Bolun Thompson <me@bolun.dev>
2024-10-05 11:15:53 -0700

    finish up to 6.2

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Yet Another CUDA Raytracer
 
 Based on [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html).
+
+## Header only?
+It's how the book is designed, and it's a hassle to change it.
+If I was optimizing this for performance, I'd do so (to avoid unnecessary inlines).
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "kvm sectorlisp";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      stdenv = pkgs.llvmPackages_19.stdenv;
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell.override {inherit stdenv;} {
+        name = "KVM Sectorlisp Shell";
+        packages = with pkgs; with llvmPackages_19; [
+          gdb
+          blink
+          binutils
+          git
+          clang-tools
+          meson
+          ninja
+          pkg-config
+        ];
+      };
+      # defaultPackage.x86_64-linux = stdenv.mkDerivation {
+      #   name = "KVM Sectorlisp";
+      #   version = "1.0.0";
+      #   src = ./.;
+      # };
+    };
+}
